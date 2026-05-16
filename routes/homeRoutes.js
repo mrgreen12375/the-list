d544c63fea022ac9aa44937fc54730af30402f46
Steven Green <stevengreen@Stevens-MacBook-Pro.local>
2026-05-15 22:21:30 -0700

    starter files

---
+++ routes/homeRoutes.js
@@ -0,0 +1,12 @@
+const home = require('express').Router();
+const path = require('path');
+
+home.get('/list', (req, res) =>{
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+  });
+  
+home.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '../public/index.html'))
+);
+
+module.exports = home
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+// const apiRoutes = require('./routes/apiRoutes');
+const homeRoutes = require('./routes/homeRoutes');
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static('public'));
+
+// app.use('/api', apiRoutes);
+app.use('/', homeRoutes);
+
+app.listen(PORT, () =>
+    console.log(`APP listening at http://localhost:${PORT}`)
+);
